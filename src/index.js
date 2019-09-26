24cbc297c1ac0901c4e0072c5fdb5f80c3898e10
Antonio <antonioroic@gmail.com>
2019-09-27 00:38:24 +0200

    Small syntax corrections

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from './components/Form';
-import {store} from './redux/actions/form.actions'
-import {Provider} from 'react-redux';
+import { store } from './redux/actions/form.actions'
+import { Provider } from 'react-redux';
 
 
 ReactDOM.render(
